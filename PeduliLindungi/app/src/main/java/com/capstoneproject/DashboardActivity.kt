1506a83d4e698147129624f07f33805d50acfd11
amirahauliaf <airaaulia25@gmail.com>
2021-05-29 22:05:17 +0700

    Dashboard Activity

--- PeduliLindungi/app/src/main/java/com/capstoneproject/DashboardActivity.kt
+++ PeduliLindungi/app/src/main/java/com/capstoneproject/DashboardActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
@@ -13,13 +14,28 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        val btn_logout:Button = findViewById(R.id.btn_logout)
+        val rl_screening: RelativeLayout = findViewById(R.id.rl_screening)
+        val rl_history: RelativeLayout = findViewById(R.id.rl_history)
+        val rl_about_us: RelativeLayout = findViewById(R.id.rl_about_us)
+        val rl_logout: RelativeLayout = findViewById(R.id.rl_logout)
 
-        btn_logout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            startActivity(Intent(applicationContext,LoginActivity::class.java))
-            finish()
+        rl_screening.setOnClickListener {
+
         }
 
+        rl_history.setOnClickListener {
+
+        }
+
+        rl_about_us.setOnClickListener {
+
+        }
+
+        rl_logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(applicationContext,LoginActivity::class.java))
+            Toast.makeText(this,"Logout success.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
